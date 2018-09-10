74ff38588bd4fae5db7a70d55b7114ea882163d7
cmarzinzik <cmarzinzik@hotmail.ca>
2018-09-10 13:49:07 -0700

    A basic API pull to get weather from an API and print all the information out

---
+++ get-weather.py
@@ -0,0 +1,14 @@
+import requests
+#my API key
+apiToken = '508a278f88f9373234be3942c8748ee5/'
+#base API url
+apiUrl = 'https://api.darksky.net/forecast/'
+#Longitude and Latitude for Sooke BC
+longatude = '48.3742576,'
+latitude = '-123.7388384'
+
+apiUrl = apiToken + apiUrl + longatude +latitude
+#Get information from API
+r = requests.get(url=apiUrl)
+#print the info
+print(r.json())
