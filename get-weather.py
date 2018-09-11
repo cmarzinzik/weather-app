ff1a140b7fdb5d0b5e91333adbf56eed02dcc574
cmarzinzik <cmarzinzik@hotmail.ca>
2018-09-11 00:37:05 -0700

    Changed everything to display current time and date, weather and temperature info
    next change will be to send a text message or tweet to myself with the info

--- get-weather.py
+++ get-weather.py
@@ -1,4 +1,6 @@
+from datetime import datetime, timedelta
 import requests
+import json
 #my API key
 apiToken = '508a278f88f9373234be3942c8748ee5/'
 #base API url
@@ -8,7 +10,23 @@ longatude = '48.3742576,'
 latitude = '-123.7388384'
 
 apiUrl = apiUrl + apiToken +longatude + latitude
-#Get information from API
+
+#Get information from API and turn it into a dictionary so we can search the json object
 r = requests.get(url=apiUrl)
-#print the info
-print(r.json())
+jsonOutput = json.loads(r.text)
+
+#Parse time and date from the API and minus seven hours to account for the time zone difference
+time = jsonOutput['currently']['time']
+timeOutput = (datetime.fromtimestamp(time) - timedelta(hours=7)).strftime('%I:%M %p')
+date = (datetime.fromtimestamp(time)- timedelta(hours=7)).strftime('%A %d %B')
+
+#Parse weather data from the json
+summary = jsonOutput['currently']['summary']
+icon = jsonOutput['currently']['icon']
+temp = jsonOutput['currently']['temperature']
+
+#convert temperature to celcius
+temp = (int(temp) - 32) * .5556
+
+#display the data
+print("On " + date + " at " + timeOutput + " it is currently " + summary + " and " + str(round(temp)) + " degrees celcius in Sooke BC Canada")
